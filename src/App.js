82eabd33d091c1c770b2d86ae5d2fcfd37b79ae9
suminlee <suminpixel@gmail.com>
2020-01-12 06:52:37 +0900

    gh-pages setting

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import imgWebpack from './images/p_ic_webpack.png';
 import imgZepline from './images/p_ic_zepline.png';
 import imgSnuperCover from './images/p_img_snuper_cover.png'
 import imgDeco1 from './images/p_img_deco_1.png'
-import imgSnuper1 from './images/p_img_snuper_1.png'
+//import imgSnuper1 from './images/p_img_snuper_1.png'
 import imgSnuper2 from './images/p_img_snuper_2.png'
 import imgSnuper3 from './images/p_img_snuper_3.png'
 import imgSnuper4 from './images/p_img_snuper_4.png'
@@ -27,6 +27,7 @@ import imgSnuper5 from './images/p_img_snuper_5.png'
 import imgSnuper6 from './images/p_img_snuper_6.png'
 import imgSnuper8 from './images/p_img_snuper_8.png'
 import imgSnuper0 from './images/p_img_snuper_0.png'
+
 const App = () => {
     const [isPopupOpen, setIsPopupOpen ] = useState(false);
 
@@ -34,11 +35,11 @@ const App = () => {
         console.log(`click : ${value}`);
         setIsPopupOpen(true);
 
-    },[isPopupOpen]);
+    },[]);
     const onClickBackground = useCallback((e) => {
         e.preventDefault();
         setIsPopupOpen(false);
-    },[isPopupOpen])
+    },[]);
   return (
       <div>
         <div style={{backgroundColor:'#463AF5', paddingTop:84, paddingBottom:84, textAlign:'center'}}>
@@ -66,64 +67,64 @@ const App = () => {
                   {/*<p style={{color:'#463AF5', marginTop:80}}>Skills & Stack</p>*/}
                   <div style={{marginTop:100}}>
                       <div style={{ display:'inline-block', textAlign:'center', width:60,  marginRight:20}}>
-                          <img style={{width:60}} src={imgHtml}/>
+                          <img  alt="image1" style={{width:60}} src={imgHtml}/>
                           <p style={{marginTop:7, fontSize:12}}>HTML</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgCss}/>
+                          <img  alt="image2" style={{width:60}} src={imgCss}/>
                           <p style={{marginTop:7, fontSize:12}}>CSS</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgJs}/>
+                          <img  alt="image3" style={{width:60}} src={imgJs}/>
                           <p style={{marginTop:7, fontSize:12}}>JS (ES6)</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgTs}/>
+                          <img alt="image4" style={{width:60}} src={imgTs}/>
                           <p style={{marginTop:7, fontSize:12}}>TypeScript</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgSass}/>
+                          <img  alt="image5" style={{width:60}} src={imgSass}/>
                           <p style={{marginTop:7, fontSize:12}}>SASS</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgReact}/>
+                          <img alt="image6"  style={{width:60}} src={imgReact}/>
                           <p style={{marginTop:7, fontSize:12}}>React</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgRedux}/>
+                          <img  alt="image7" style={{width:60}} src={imgRedux}/>
                           <p style={{marginTop:7, fontSize:12}}>Redux</p>
                       </div>
                       <br/><br/>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgAws}/>
+                          <img alt="image8"  style={{width:60}} src={imgAws}/>
                           <p style={{marginTop:7, fontSize:12}}>AWS</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgNode}/>
+                          <img alt="image9"  style={{width:60}} src={imgNode}/>
                           <p style={{marginTop:7, fontSize:12}}>Node.js</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgExpress}/>
+                          <img alt="image10"  style={{width:60}} src={imgExpress}/>
                           <p style={{marginTop:7, fontSize:12}}>Express.js</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgWebpack}/>
+                          <img alt="image11"  style={{width:60}} src={imgWebpack}/>
                           <p style={{marginTop:7, fontSize:12}}>Webpack</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgMysql}/>
+                          <img alt="image12"  style={{width:60}} src={imgMysql}/>
                           <p style={{marginTop:7, fontSize:12}}>MySQL</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgGit}/>
+                          <img alt="image13"  style={{width:60}} src={imgGit}/>
                           <p style={{marginTop:7, fontSize:12}}>git</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgSketch}/>
+                          <img alt="image14"  style={{width:60}} src={imgSketch}/>
                           <p style={{marginTop:7, fontSize:12}}>Sketch</p>
                       </div>
                       <div style={{ display:'inline-block', textAlign:'center', width:60, marginRight:20}}>
-                          <img style={{width:60}} src={imgZepline}/>
+                          <img alt="image15"  style={{width:60}} src={imgZepline}/>
                           <p style={{marginTop:7, fontSize:12}}>Zepline</p>
                       </div>
                   </div>
@@ -133,14 +134,13 @@ const App = () => {
               <div style={{width:1080, display:'inline-block', textAlign:'left', paddingLeft:20}}>
                   <h1 style={{color:'white'}}>웹 프론트엔드 개발자로 일하며<br/>다음과 같은 <span style={{color:'#65FF7F'}}> 멋진 프로젝트 </span>경험을 쌓았습니다.</h1>
                   <div style={{marginTop:38, display: 'flex', flexBasis:'50%'}}>
-                        <div style={{width:206, cursor:'pointer', flex:1 }} id="snuper" onClick={(e) => onClickThisProject(1)}>
-                            <img style={{width:206, marginBottom:19, paddingRight:24}} src={imgSnuperCover}/>
+                        <div style={{width:206, cursor:'pointer', flex:1 }} id="snuper" onClick={() => onClickThisProject(1)}>
+                            <img alt="image16" style={{width:206, marginBottom:19, paddingRight:24}} src={imgSnuperCover}/>
                             <h4 style={{color:'white', margin:0}}>Snuper (스누퍼)</h4>
                             <p style={{color:'#cccccc', marginTop:8}}>리액트 웹 프론트엔드 개발<br/> 업무 및 과업관리 웹 서비스</p>
                         </div>
                   </div>
               </div>
-
           </div>
           <div style={{backgroundColor:'white',paddingTop:80, paddingBottom:100, textAlign:'center'}}>
               <div style={{width:1080, display:'inline-block',position:'relative', textAlign:'left', paddingLeft:20}}>
@@ -156,7 +156,7 @@ const App = () => {
                       현재는 AI 솔루션 스타트업 (주)시즐에서 웹 프론트엔드 개발자로 있습니다<br/>
                       자사 인공지능 엔진, 협력사 서비스 등을 개발합니다
                   </p>
-                  <img src={imgDeco1} style={{width:500, position:'absolute', bottom:0, right:0}}/>
+                  <img alt="image17"  src={imgDeco1} style={{width:500, position:'absolute', bottom:0, right:0}}/>
                   <table style={{marinTop:26, color:'#555555', fontSize:13}}>
                       <tbody>
                           <tr style={{verticalAlign:'top'}}>
@@ -190,13 +190,12 @@ const App = () => {
           </div>
           {isPopupOpen ?
               <div>
-                  <a onClick={onClickBackground} style={{backgroundColor:'#1B324298', position:'fixed', textAlign:'center', height:'100%', zIndex:3, top:0, left:0, right:0,}}>
-
+                  <a href="/" onClick={onClickBackground} style={{backgroundColor:'#1B324298', position:'fixed', textAlign:'center', height:'100%', zIndex:3, top:0, left:0, right:0,}}>
                   </a>
                   <div className="div-modal-dashboard" style={{position:'fixed', textAlign:'center', height:'100%', zIndex:4, top:0, left:'50%', transform:'translate(-50%, 0)'}}>
 
                           <div className="div-white-bg-view-task-body" style={{position:'relative', width:'100%', height: `calc(100vh - 120px)`}} >
-                              <div className="div-bg-image-1" style={{position:'absolute', width:'100%',height:364, }}></div>
+                              <div className="div-bg-image-1" style={{position:'absolute', width:'100%',height:364, }}>&nbsp;</div>
                               {/*
                               <div style={{position:'absolute', backgroundColor:'#00000095', width:'100%',height:365}}>
                                   <h2 style={{color:'white', paddingRight:10, fontSize:28}}>스누퍼 프리미엄 플랜</h2>
@@ -245,31 +244,31 @@ const App = () => {
                                   </table>
                                   <div style={{width:'100%',display: 'flex', flexBasis:'50%', marginTop:66}}>
                                       <div style={{width:'50%',cursor:'pointer', flex:1, padding:12 }} >
-                                          <img style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper0}/>
+                                          <img  alt="image18" style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper0}/>
                                       </div>
                                       <div style={{width:'50%',cursor:'pointer', flex:1 , padding:12 }} >
-                                          <img style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper8}/>
+                                          <img  alt="image19" style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper8}/>
                                       </div>
                                   </div>
                                     <div style={{width:'100%',display: 'flex', flexBasis:'50%'}}>
                                         <div style={{width:'100%',cursor:'pointer', flex:1 , padding:12 }} >
-                                            <img style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper3}/>
+                                            <img  alt="image20" style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper3}/>
                                         </div>
                                     </div>
                                     <div style={{width:'100%',display: 'flex', flexBasis:'50%'}}>
                                         <div style={{width:'50%',cursor:'pointer', flex:1, padding:12 }} >
-                                            <img style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper2}/>
+                                            <img  alt="image21" style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper2}/>
                                         </div>
                                         <div style={{width:'50%',cursor:'pointer', flex:1 , padding:12 }} >
-                                            <img style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper6}/>
+                                            <img  alt="image22" style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper6}/>
                                         </div>
                                     </div>
                                     <div style={{width:'100%',display: 'flex', flexBasis:'50%'}}>
                                         <div style={{width:'50%',cursor:'pointer', flex:1, padding:12 }} >
-                                            <img style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper4}/>
+                                            <img  alt="image23" style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper4}/>
                                         </div>
                                         <div style={{width:'50%',cursor:'pointer', flex:1 , padding:12 }} >
-                                            <img style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper5}/>
+                                            <img  alt="image24" style={{width:'100%', marginBottom:19, paddingRight:24}} src={imgSnuper5}/>
                                         </div>
                                     </div>
                               </div>
@@ -285,6 +284,6 @@ const App = () => {
 
 
   );
-}
+};
 
 export default App;
